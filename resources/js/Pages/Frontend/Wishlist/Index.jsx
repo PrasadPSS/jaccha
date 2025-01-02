fa76c496213adb54bc293b3404f03538b7aa599b
PrasadPSS <prasadi@parasightsolutions.com>
2025-01-02 15:59:32 +0530

    wishlist empty integration

--- resources/js/Pages/Frontend/Wishlist/Index.jsx
+++ resources/js/Pages/Frontend/Wishlist/Index.jsx
@@ -23,7 +23,45 @@ export default function Index({ wishlist }) {
 
     return (
         <UserMenu auth={auth} activeTab={'wishlist'}>
-            <div
+            {wishlist.length <= 0 &&
+                <div
+                    className="tab-pane fade active show"
+                    id="pills-fourth"
+                    role="tabpanel"
+                    aria-labelledby="pills-fourth-tab"
+                >
+                    <div className="my-wishlist">
+                        <div className="my-wishlist-box mb-4">
+                            <h4>My Wish list</h4>
+
+                        </div>
+                        <div className="row align-items-center">
+
+                            <div className="col-md-6">
+                                <div className="empty-wishlist-content">
+                                    <h2>Your Wish List is Empty!</h2>
+                                    <p>
+                                        Looks like you haven’t added anything to your wishlist yet.
+                                        Start exploring our wide range of products and save your favorites for later!
+                                    </p>
+                                    <Link as='button' href='/products' className="btn btn-primary">Start Shopping</Link>
+                                </div>
+                            </div>
+
+
+                            <div className="col-md-6">
+                                <div className="empty-wishlist-image">
+                                    <img
+                                        src="/assets/images/empty-wishlist.png"
+                                        alt="Empty Wishlist Illustration"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>}
+            {wishlist.length > 0 && <div
                 className="tab-pane fade show active"
                 id="pills-fourth"
                 role="tabpanel"
@@ -38,34 +76,35 @@ export default function Index({ wishlist }) {
                     <div className="row g-5" data-aos="fade-up">
                         {wishlist.length > 0 && wishlist.map((item, index) => (
                             <Link className="col-sm-4" as='a' href={"/product/view/" + item.products.product_id} key={index}>
-                        
-                                    <div className="feature-box">
-                                        <div className="basket-box">
-                                            <Link href="/product/addtocart" as='a' method="post" id="addtoBasketButton" data={{ product_id: item.products.product_id, product_type:  "simple", product_variant_id: null, quantity: 1, _token: document.querySelector('meta[name="csrf-token"]').getAttribute('content'), sweetnesslevel: 'low', ingrediantaddons: 'NA', exclusions: 'NA' }}>
-                                            <i  className="fal fa-shopping-basket"></i>
-                                            </Link>
-                                            
-                                        </div>
 
-                                        <i onClick={() => handleDeleteFromWishlist(item.products.product_id)} className="fas fa-heart heart"></i>
-                                        <img
-                                            src={'/backend-assets/uploads/product_thumbs/' + item.products.product_thumb}
-                                            alt="feature image"
-                                        />
-                                        <div className="features-content">
-                                            <h5>{item.products.product_title}</h5>
-                                            <h6>₹{item.products.product_price}.00</h6>
-                                        </div>
+                                <div className="feature-box">
+                                    <div className="basket-box">
+                                        <Link href="/product/addtocart" as='a' method="post" id="addtoBasketButton" data={{ product_id: item.products.product_id, product_type: "simple", product_variant_id: null, quantity: 1, _token: document.querySelector('meta[name="csrf-token"]').getAttribute('content'), sweetnesslevel: 'low', ingrediantaddons: 'NA', exclusions: 'NA' }}>
+                                            <i className="fal fa-shopping-basket"></i>
+                                        </Link>
+
                                     </div>
-                               
+
+                                    <i onClick={() => handleDeleteFromWishlist(item.products.product_id)} className="fas fa-heart heart"></i>
+                                    <img
+                                        src={'/backend-assets/uploads/product_thumbs/' + item.products.product_thumb}
+                                        alt="feature image"
+                                    />
+                                    <div className="features-content">
+                                        <h5>{item.products.product_title}</h5>
+                                        <h6>₹{item.products.product_price}.00</h6>
+                                    </div>
+                                </div>
+
                             </Link>
                         ))}
-                        {wishlist.length <= 0 && <div>Wishlist is empty</div>}
+                        
 
                     </div>
 
                 </div>
-            </div>
+            </div>}
+
         </UserMenu>
 
     );
