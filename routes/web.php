48f151008eaec89865b552ab3d6c06ddd6607c25
PrasadPSS <prasadi@parasightsolutions.com>
2025-01-04 10:44:22 +0530

    fix

--- routes/web.php
+++ routes/web.php
@@ -82,7 +82,7 @@ use App\Http\Controllers\backend\TrafficsourceController; //22-12-2022
 
 use App\Http\Controllers\Frontend\PagesController;
 use App\Http\Controllers\Frontend\FaqController;
-use App\Http\Controllers\frontend\UsersController;
+use App\Http\Controllers\Frontend\UsersController;
 use App\Http\Controllers\frontend\WishlistsController;
 use App\Http\Controllers\frontend\SimilarController;
 use App\Http\Controllers\frontend\MyaccountController;
