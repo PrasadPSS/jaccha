63b50e1e7a17545ca2bc8fc4739f8e976dd9e403
PrasadPSS <prasadi@parasightsolutions.com>
2025-01-21 15:05:11 +0530

    remove breadcrumbs with wrong links

--- resources/views/frontend/orders/viewinvoice.blade.php
+++ resources/views/frontend/orders/viewinvoice.blade.php
@@ -17,17 +17,7 @@
       <div class="content-header row">
           <div class="content-header-left col-12 mb-2 mt-1">
             <div class="row breadcrumbs-top">
-              <div class="col-12">
-                <h5 class="content-header-title float-left pr-1 mb-0">Orders Invoice</h5>
-                <div class="breadcrumb-wrapper col-12">
-                  <ol class="breadcrumb p-0 mb-0">
-                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard')}}"><i class="bx bx-home-alt"></i></a></li>
-                    <li class="breadcrumb-item"><a href="{{ route('admin.orders')}}">Orders</a></li>
-                    <li class="breadcrumb-item active">Order Invoice
-                    </li>
-                  </ol>
-                </div>
-              </div>
+              
             </div>
           </div>
         </div>
